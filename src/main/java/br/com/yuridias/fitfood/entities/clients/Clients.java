5be5db4d35338bb4caa9757133f2196c409ca670
Yuri Dias <yuri.dazevedo@gmail.com>
2021-05-31 20:07:07 -0300

    feat: client model

--- src/main/java/br/com/yuridias/fitfood/entities/clients/Clients.java
+++ src/main/java/br/com/yuridias/fitfood/entities/clients/Clients.java
@@ -1,4 +1,14 @@
 package br.com.yuridias.fitfood.entities.clients;
 
 public class Clients {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Long latitude;
+    private Long longitude;
+
 }
